6b06e947d10c285ee9987e1f65cb7932eaba5d27
Zola Gonano <zolagonano@protonmail.com>
2023-07-12 15:22:10 +0000

    Implement PoWAlgo

--- anonid_pow/src/lib.rs
+++ anonid_pow/src/lib.rs
@@ -1,3 +1,5 @@
+use sha2::{Digest, Sha256};
+
 pub struct UserData {
     username: String,
     auth_address: String,
@@ -32,3 +34,23 @@ impl UserData {
     }
 }
 
+pub enum PoWAlgo {
+    Sha256,
+}
+
+impl PoWAlgo {
+    pub fn calculate(&self, userdata: &str, nonce: usize) -> String {
+        match self {
+            PoWAlgo::Sha256 => {
+                let mut hasher = Sha256::new();
+                hasher.update(userdata.as_bytes());
+
+                hasher.update(format!(":{nonce}").as_bytes());
+
+                let final_hash = hasher.finalize();
+
+                format!("{:x}", final_hash)
+            }
+        }
+    }
+}
